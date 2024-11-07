4f03ff2c303418fb1463c96368fdde07ceb4921c
fabrice <fabrice@fabrice.com>
2024-11-07 16:01:37 +0100

    passage de parametre variable mutable

--- passage_parametres.py
+++ passage_parametres.py
@@ -10,7 +10,8 @@ def affiche_prix(prix, taux):
 affiche_prix(100, 10)
 affiche_prix(110, 20)
 
-#passage de paramètres par référence
+#passage de paramètres par référence objet  immuable
+print("Tentative de passage de paramètres par référence objet immuable")
 def reduction(prix, taux):
     prix = prix * (1 - taux/100)    #prix est une variable locale
     print("Prix dans la fonction: ", prix)
@@ -19,9 +20,28 @@ def affiche_prix(prix, taux):
     print("Prix: ", prix)
     print("Taux: ", taux)
     reduction(prix, taux)
-    
+
     print("Prix après réduction: ", prix)
 
 #exemple d'appel de la fonction
 affiche_prix(100, 10)
 affiche_prix(110, 20)
+
+#passage de paramètres par référence objet mutable
+print("Tentative de passage de paramètres par référence objet mutable")
+def reduction(liste, taux):
+    liste[0] = liste[0] * (1 - taux/100)    #liste est une variable locale
+    print("Prix dans la fonction: ", liste[0])
+
+def affiche_prix(liste, taux):
+    print("Prix: ", liste[0])
+    print("Taux: ", taux)
+    reduction(liste, taux)
+
+    print("Prix après réduction: ", liste[0])       #liste est modifiée par la fonction reduction
+
+#exemple d'appel de la fonction
+liste = [100, 10]
+affiche_prix(liste, 10)
+affiche_prix(liste, 20)
+
