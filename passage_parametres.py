55d3ee9e561db46ec3bf181734f527daf21fdc6d
fabrice <fabrice@fabrice.com>
2024-11-07 15:09:48 +0100

    passage de parametre

--- passage_parametres.py
+++ passage_parametres.py
@@ -9,3 +9,19 @@ def affiche_prix(prix, taux):
 #exemple d'appel de la fonction
 affiche_prix(100, 10)
 affiche_prix(110, 20)
+
+#passage de paramètres par référence
+def reduction(prix, taux):
+    prix = prix * (1 - taux/100)    #prix est une variable locale
+    print("Prix dans la fonction: ", prix)
+
+def affiche_prix(prix, taux):
+    print("Prix: ", prix)
+    print("Taux: ", taux)
+    reduction(prix, taux)
+    
+    print("Prix après réduction: ", prix)
+
+#exemple d'appel de la fonction
+affiche_prix(100, 10)
+affiche_prix(110, 20)
