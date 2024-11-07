5f039b6055ac05314fcda0f73eafc39365ecc473
fabrice <fabrice@fabrice.com>
2024-11-07 13:52:37 +0100

    objet immutable

--- mutable.py
+++ mutable.py
@@ -1,7 +1,9 @@
 prenom = "patric"
 print(prenom)
 print("emplacement mémoire de prénom: ", id(prenom))
+print("type de prénom: ", type(prenom))
 
 prenom += "e"
 print(prenom)
 print("emplacement mémoire de prénom: ", id(prenom))
+print("type de prénom: ", type(prenom))
